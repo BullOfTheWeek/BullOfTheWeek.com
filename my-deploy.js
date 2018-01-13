e3e1418082c9134744c79a8f8a43c306726b61b9
milad asghari <miladasghari2016@gmail.com>
2018-01-14 01:42:53 +0330

    deploy donegit branchgit branch npm run my-deploy and npm run my-dev-deploy

--- my-deploy.js
+++ my-deploy.js
@@ -1,2 +1,20 @@
+
 require('dotenv/config');
-console.log(process.env.test);
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+	username: process.env.username,
+	password: process.env.password, // optional, prompted if none given
+	host: process.env.host,
+	port: 21,
+	localRoot: __dirname + "/static",
+	remoteRoot: "bulloftheweek.com/BullOfTheWeek/master/",
+	include: ['static/*'],
+	exclude: ['.*', 'tmp/*', 'build/*','node_modules/*','node_modules',]
+}
+	
+ftpDeploy.deploy(config, function(err) {
+	if (err) console.log(err)
+	else console.log('finished');
+});
---
+++ my-dev-deploy.js
@@ -0,0 +1,20 @@
+
+require('dotenv/config');
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+var config = {
+	username: process.env.username,
+	password: process.env.password, // optional, prompted if none given
+	host: process.env.host,
+	port: 21,
+	localRoot: __dirname + "/static",
+	remoteRoot: "bulloftheweek.com/BullOfTheWeek/develop/",
+	include: ['static/*'],
+	exclude: ['.*', 'tmp/*', 'build/*','node_modules/*','node_modules',]
+}
+	
+ftpDeploy.deploy(config, function(err) {
+	if (err) console.log(err)
+	else console.log('finished');
+});
