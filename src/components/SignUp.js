336e13dc834a2548b5313229dd9c9355a0f27358
milad asghari <miladasghari2016@gmail.com>
2018-01-24 12:49:51 +0330

    add subscribe form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,32 @@
 import React from 'react'
-
-
+import styles from './SignUp.module.css'
+import 'typeface-vt323'
 class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+        this.handleChange = this.handleChange.bind(this);
+      }
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
     render() {
         return (
             <div style={{textAlign:'center'}}>
-                <h2> join us ! </h2>
+                <h1 style={{fontFamily:'vt323'}}> join us ! </h1>
+                <form id="signupSubscriberForm" method="post" action="https://bulloftheweek.campayn.com/contacts/signup_form_add_contact/71708" acceptCharset="utf-8">
+                    <input type="hidden" name="formId" value="71280" />
+                    <div style={{marginTop:'2rem'}}>
+                      <span>Fill out your e-mail address <br /> to receive our newsletter!</span>
+                    </div>
+                    <div style={{marginTop:'2rem'}}>
+                      <input type="email" id="emailAddress" value={this.state.value} onChange={this.handleChange} name="email" placeholder="youremail@gmail.com" />
+                    </div>
+                    <div style={{marginTop:'2rem'}}>
+                      <input className={styles.button} type="submit" id="subscribeToList" value="Subscribe" />
+                    </div>
+                    <a href="https://campayn.com">campayn</a>
+                </form>
             </div>
         )
     }
