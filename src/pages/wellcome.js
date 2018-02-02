30ee6b2baf75e1717e63fba6aa838270914a4633
milad asghari <miladasghari2016@gmail.com>
2018-02-02 19:20:47 +0330

    add welcome for auth twitter

---
+++ src/pages/wellcome.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import Bio from '../components/Bio'
+import SignUp from '../components/SignUp'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Blog from '../components/Blog'
+import { rhythm } from '../utils/typography'
+import styles from './index.module.css'
+import Headroom from 'react-headroom'
+import { slide as Menu } from 'react-burger-menu'
+import 'typeface-vt323'
+
+class wellcome extends React.Component {
+
+
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+    return (      
+      <div style={{marginTop:'5rem'}}>
+        <Helmet title="Thank you" />
+        <div>
+           <a href="/"><h1>Bull Of The Week</h1> </a> 
+           <p>Thanks for register </p>
+        </div>
+        <footer>
+          <div style={{textAlign:"center",marginTop:"6em",fontSize:"13px"}}>
+             <span>made with love in 2018</span>
+          </div> 
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default wellcome
+
