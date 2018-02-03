b1ced31c1d8435723d1f88e647d022358d7ee83a
milad asghari <miladasghari2016@gmail.com>
2018-02-03 10:22:51 +0330

    use Twitteroauth and return authorize link in twitter_login.php

--- backend/twitter_login.php
+++ backend/twitter_login.php
@@ -1,14 +1,28 @@
-
- <?php
-   require "vendor/autoload.php";
-   use Abraham\TwitterOAuth\TwitterOAuth;
-   $config = require_once 'config.php';
-   define('CONSUMER_KEY', $config['consumer_key']);
-   define('CONSUMER_SECRET', $config['consumer_secret']);
-   define('OAUTH_CALLBACK', $config['url_callback']);
-
-   $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-   $request_token = $connection->oauth('oauth/request_token', array('oauth_callback' => OAUTH_CALLBACK));
-   $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token']));
-   var_dump($url);
-  ?>
+<?php
+require_once 'vendor/autoload.php';
+use Abraham\TwitterOAuth\TwitterOAuth;
+session_start();
+$config = require_once 'config.php';
+// create TwitterOAuth object
+$twitteroauth = new TwitterOAuth($config['consumer_key'], $config['consumer_secret']);
+// request token of application
+$request_token = $twitteroauth->oauth(
+    'oauth/request_token', [
+        'oauth_callback' => $config['url_callback']
+    ]
+);
+// throw exception if something gone wrong
+if($twitteroauth->getLastHttpCode() != 200) {
+    throw new \Exception('There was a problem performing this request');
+}
+// save token of application to session
+$_SESSION['oauth_token'] = $request_token['oauth_token'];
+$_SESSION['oauth_token_secret'] = $request_token['oauth_token_secret'];
+// generate the URL to make request to authorize our application
+$url = $twitteroauth->url(
+    'oauth/authorize', [
+        'oauth_token' => $request_token['oauth_token']
+    ]
+);
+// and redirect
+header('Location: '. $url);
