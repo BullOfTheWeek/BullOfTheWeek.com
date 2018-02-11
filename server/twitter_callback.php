548846a48ac162fc28ec906bd7c80c9ad169d2f6
milad asghari <miladasghari2016@gmail.com>
2018-02-11 08:51:28 +0330

    add cookies to server

--- server/twitter_callback.php
+++ server/twitter_callback.php
@@ -32,17 +32,44 @@
 
     if(mysqli_num_rows($query) > 0){
 
-       // echo "email already exists";
-      header('Location: http://www.bulloftheweek.com/');
+      //  echo "email already exists";
+    //  $conn->close();
+    //  header('Location: http://www.bulloftheweek.com/');
+    $row = mysqli_fetch_array($query);
+    $subscribed_date = $row['subscribed_date'];
+    $membership_finishes = $row['membership_finishes'];
+    $membership = $row['membership'];
+    setcookie("subscribed_date", $subscribed_date, time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("membership_finishes", $membership_finishes, time() + (86400 * 30), "/"); // 86400 = 1 day
+    setcookie("membership", $membership, time() + (86400 * 30), "/"); // 86400 = 1 day
+    $conn->close();
+    header('Location: http://www.bulloftheweek.com/develop/dash-alert');
 
     }
     else{
 
-      $sql = "INSERT INTO alertUsers (username,subscribed_date)
-      VALUES ('".$access_token['screen_name']."', CURRENT_TIMESTAMP)";
+      $sql = "INSERT INTO alertUsers (username,subscribed_date,membership,membership_finishes)
+      VALUES ('".$access_token['screen_name']."', CURRENT_TIMESTAMP, 'beta',DATE_ADD(NOW(), INTERVAL 15 DAY))";
       if ($conn->query($sql) === TRUE) {
         //  echo "New record created successfully";
-        header('Location: http://www.bulloftheweek.com/welcome');
+        $finder = mysqli_query($conn, "SELECT * FROM alertUsers WHERE username='".$access_token['screen_name']."'");
+        if (!$finder)
+        {
+            die('Error: ' . mysqli_error($conn));
+        }
+        if(mysqli_num_rows($finder) > 0){
+          $row = mysqli_fetch_array($finder);
+          $subscribed_date = $row['subscribed_date'];
+          $membership_finishes = $row['membership_finishes'];
+          $membership = $row['membership'];
+          setcookie("subscribed_date", $subscribed_date, time() + (86400 * 30), "/"); // 86400 = 1 day
+          setcookie("membership_finishes", $membership_finishes, time() + (86400 * 30), "/"); // 86400 = 1 day
+          setcookie("membership", $membership, time() + (86400 * 30), "/"); // 86400 = 1 day
+          $conn->close();
+          header('Location: http://www.bulloftheweek.com/develop/dash-alert');
+        }
+
+
       } else {
           echo "Error: " . $sql . "<br>" . $conn->error;
       }
