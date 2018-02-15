e4a37d03dc3c9386d834ef4847228c5bc0a5344f
milad asghari <miladasghari2016@gmail.com>
2018-02-15 11:45:54 +0330

    update config.php

--- server/config.php
+++ server/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'consumer_key'      => 'aBsPiOHXXeYvAdVcL27IWwoX1',
-    'consumer_secret'   => 'ki3YvpoLGp30KGvXdSsZP6B7iVFfJbiH3sxdw8zpXlDRbolVhL',
+    'consumer_key'      => '',
+    'consumer_secret'   => '',
     'url_login'         => '',
-    'url_callback'      => 'http://BullOfTheWeek.com/server/twitter_callback.php',
-    'servername' => "localhost",
-    'username' => "bullixty_root",
-    'password' => "8cLyU@Fc893K",
-    'dbname' => "bullixty_BullOfTheWeek",
+    'url_callback'      => '',
+    'servername' => "",
+    'username' => "",
+    'password' => "",
+    'dbname' => "",
 ];
